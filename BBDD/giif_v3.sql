e12a8f64c25ba67e3efba596bd384b366e6ca37f
aimenrial432 <oier.garcia@maristak.net>
2021-10-19 11:50:53 +0200

    Modelo relacional BBDD y modificaciones de script

--- BBDD/giif_v3.sql
+++ BBDD/giif_v3.sql
@@ -1,3 +1,6 @@
+drop database if exists giif_v3;
+
+
 create database giif_v3;
 
 use giif_v3;
@@ -11,15 +14,15 @@ create table Empleados (
     Password_hash varchar(255) not null,
     Nombre varchar(50) not null,
     Apellido varchar(50) not null,
-    Depatamento_ID int 
+    Departamento_Id int,
+    Id_rol int
 
 );
 
 -- Tabla Roles--------------------
 
 create table Roles (
-	
-    DNI varchar(9),
+	Id_rol int auto_increment primary key,
     Rol varchar(20) not null
 
 );
@@ -38,7 +41,7 @@ create table Conexiones (
 
 create table Departamentos (
 	
-    Departamento_ID int auto_increment,
+    Departamento_Id int auto_increment primary key,
     Nombre varchar(50) not null
 
 );
@@ -47,7 +50,7 @@ create table Departamentos (
 
 create table Proyectos (
 	
-    Proyecto_ID int auto_increment,
+    Proyecto_Id int auto_increment primary key,
     Descripcion varchar(255),
     Fecha_inicio Date not null,
     Fecha_fin Date
@@ -59,10 +62,93 @@ create table Proyectos (
 create table Integrantes (
 	
     Proyecto_ID int,
-    DNI varchar(9) 
+    DNI varchar(9)
 
 );
 
 -- Tabla Registro de horas ----------------
 
-create table 
+CREATE TABLE Registro_horas (
+	DNI varchar(9),
+	Dia date not null,
+	Semana int(11) not null,
+	Horas float not null,
+	Proyecto_Id int(11)
+    
+    );
+    
+    -- Tabla gastos------------------
+   CREATE TABLE Gastos (
+	Gasto_Id int(11) auto_increment primary key,
+	Proyecto_Id int(11),
+	DNI varchar(9),
+	Pais varchar(50) not null,
+	Ciudad varchar(50) not null,
+	Fecha date not null,
+	Costo decimal(7,2) not null 
+  
+  );
+  
+  -- Tabla dietas
+  
+  CREATE TABLE Dietas (
+	Gasto_Id int(11),
+	Tipo varchar(50) not null,
+	Descripcion varchar(255) not null,
+	Region varchar(20) not null
+    
+);
+
+-- Tabla transporte--------------
+CREATE TABLE Transporte (
+	Gasto_Id int(11),
+	Medio varchar(50) not null,
+	Distancia decimal(7,2) not null,
+	Peaje decimal(7,2) not null,
+	Parking decimal(7,2) not null
+  
+);
+
+
+-- Primary keys ---------------------------------------
+
+-- tabla Empleados-----------------
+alter table Empleados add primary key (DNI);
+
+-- Keys tabla integrantes----------------------
+alter table Integrantes add primary key (Proyecto_Id, DNI);
+
+-- Keys tabla conexiones-----------------
+alter table Conexiones add primary key(DNI, Fecha);
+
+-- keys tabla registro horas
+alter table Registro_horas add primary key (DNI, Dia);
+
+
+
+-- Foreign keys------------------------------------------
+
+-- tabla Empleados-----------------
+alter table Empleados add foreign key (Id_rol) references Roles(Id_rol);
+alter table Empleados add foreign key (Departamento_Id) references Departamentos(Departamento_Id);
+
+-- Keys tabla integrantes----------------------
+alter table Integrantes add foreign key (Proyecto_Id) references Proyectos(Proyecto_Id);
+alter table Integrantes add foreign key (DNI) references Empleados(DNI);
+
+-- Keys tabla conexiones-----------------
+alter table Conexiones add foreign key (DNI) references Empleados(DNI);
+
+-- keys tabla registro horas
+alter table Registro_horas add foreign key (DNI) references Empleados(DNI);
+alter table Registro_horas add foreign key (Proyecto_Id) references Proyectos(Proyecto_Id);
+
+-- Keys tabla gastos------------------------
+alter table Gastos add foreign key (DNI) references Empleados(DNI);
+alter table Gastos add foreign key (Proyecto_Id) references Proyectos(Proyecto_Id);
+
+-- Keys tabla dietas-----------------------
+alter table Dietas add foreign key (Gasto_Id) references Gastos(Gasto_Id);
+
+-- Keys tabla transportes---------------------------
+alter table Transporte add foreign key (Gasto_Id) references Gastos(Gasto_Id);
