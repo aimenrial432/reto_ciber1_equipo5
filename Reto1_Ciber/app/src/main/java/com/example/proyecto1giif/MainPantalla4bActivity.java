cc2a612adc91add3440d4ade96146bf838300c29
aimenrial432 <oier.garcia@maristak.net>
2021-10-25 10:28:42 +0200

    Rama unai

---
+++ Reto1_Ciber/app/src/main/java/com/example/proyecto1giif/MainPantalla4bActivity.java
@@ -0,0 +1,14 @@
+package com.example.proyecto1giif;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainPantalla4bActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_pantalla4b);
+    }
+}
