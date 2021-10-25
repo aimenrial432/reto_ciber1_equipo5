e2a6027ab05138d8c806a9469287e847df00f6b5
aimenrial432 <oier.garcia@maristak.net>
2021-10-25 13:26:38 +0200

    correccion omega

---
+++ Reto1_Ciber/app/src/main/java/com/example/proyecto1giif/Insert_main_Gasto.java
@@ -0,0 +1,14 @@
+package com.example.proyecto1giif;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Insert_main_Gasto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.insert_main_gasto);
+    }
+}
---
+++ Reto1_Ciber/app/src/main/java/com/example/proyecto1giif/Search_Gastos.java
@@ -0,0 +1,14 @@
+package com.example.proyecto1giif;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Search_Gastos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.Search_Gastos);
+    }
+}
